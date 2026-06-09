5dfbc85c9f7c4ef1796436bc7aef034130cf709b
realgheart-art <realgheart@gmail.com>
2026-06-09 12:23:59 +0800

    index.html

---
+++ service_worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 's4pd-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker dan simpan cache asas
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch data (Network First, fallback to Cache jika tiada internet)
+self.addEventListener('fetch', event => {
+  // Hanya proses GET requests
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// Update Service Worker jika ada versi baharu
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
